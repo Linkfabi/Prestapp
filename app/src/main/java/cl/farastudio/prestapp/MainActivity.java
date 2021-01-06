76309292f18c257c6c41f890d9a006bfb048f7a8
Fabian Astorga <fabian.astorga02@inacapmail.cl>
2021-01-06 02:39:08 -0300

    Se crean las listas de muestras
    se crea listas de cada tipo de Prestamo
    se coloca un spinner en el main para seleccionar la lista
    se mejoran detalles menores

--- app/src/main/java/cl/farastudio/prestapp/MainActivity.java
+++ app/src/main/java/cl/farastudio/prestapp/MainActivity.java
@@ -10,12 +10,18 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 import cl.farastudio.prestapp.DTO.Prestamo;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
-
+    Spinner spinner;
+    TextView texto;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +29,16 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        texto = findViewById(R.id.textoPrueba);
+
+        spinner = (Spinner) findViewById(R.id.SpPrestamoMain);
+        // Cargamos el Spinner
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.tiposPrestamos, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+        spinner.setOnItemSelectedListener(this); // con este comando puedo escuchar cada vez que se selecciona una opcion en el spinner
+                                                // se activa el metodo onItemSelected implementado mas abajo
 
         FloatingActionButton fab = findViewById(R.id.BtnPrestamo);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -32,5 +48,45 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+    }
+
+
+
+    //OnItemSelected me permite realizar una accion depende a la opcion escojida del spinner
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        switch (parent.getId()){
+            case R.id.SpPrestamoMain:
+                int seleccionado = spinner.getSelectedItemPosition();
+                if(seleccionado == 0){
+                    texto.setText("Seleccione una Lista");
+                }else if(seleccionado == 1){
+                    texto.setText("Lista Dinero No tiene Datos");
+                }else if(seleccionado == 2){
+                    texto.setText("Lista Libro No tiene Datos");
+                }else if(seleccionado == 3){
+                    texto.setText("Lista Consolas No tiene Datos");
+                }else if(seleccionado == 4){
+                    texto.setText("Lista Notebook No tiene Datos");
+                }else if(seleccionado == 5){
+                    texto.setText("Lista Celular No tiene Datos");
+                }else if(seleccionado == 6){
+                    texto.setText("Lista Art. Electronico No tiene Datos");
+                }else if(seleccionado == 7){
+                    texto.setText("Lista Art. Construccion No tiene Datos");
+                }else if(seleccionado == 8){
+                    texto.setText("Lista Otros No tiene Datos");
+                }
+                break;
+        }
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> adapterView) {
+
+    }
+
+    @Override
+    public void onPointerCaptureChanged(boolean hasCapture) {
+
     }
 }
